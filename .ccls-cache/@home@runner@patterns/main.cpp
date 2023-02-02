0bfe72b9085c2284b4f4b82cb24b8bc2d5c664ac
Dev Nayak <95752623+Devnyk@users.noreply.github.com>
2023-02-02 18:22:17 +0000

    pushing the code

---
+++ .ccls-cache/@home@runner@patterns/main.cpp
@@ -0,0 +1,27 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int rowCount, colCount;
+  cin >> rowCount;
+  cin >> colCount;
+  for (int row = 0; row < rowCount; row = row + 1) {
+    if (row == 0 || row == rowCount - 1) {
+      for (int col = 0; col < colCount; col++) {
+        cout << "* ";
+      }
+    }
+
+    else {
+      cout << "* ";
+      for (int i = 0; i < colCount - 2; i = i + 1) {
+        cout << "  ";
+      }
+      cout << "* ";
+    }
+
+    cout << endl;
+  }
+};
